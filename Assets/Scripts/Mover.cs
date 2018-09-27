ba0c208ae94db3bd137c54412f514f8d4edb1650
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-09-27 18:22:20 -0400

    Added movement and lasers

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    // Variable Declarations
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = transform.right * speed;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Variable Declaration
+    public float speed = 5.0f;
+    public float xMin, xMax, yMin, yMax;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    // Fixed update increments. Used for physics!
+    void FixedUpdate()
+    {
+        // Read input
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        Debug.Log("x: " + horiz + ", y: " + vert);
+
+        Vector2 movement = new Vector2(horiz, vert);
+
+        // Move the player
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = movement * speed;
+
+        // Restrict the player from leaving the play area
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, xMin, xMax),  // Restrict the x position to xMin and xMax
+            Mathf.Clamp(rBody.position.y, yMin, yMax)); // Restrict the y position to yMin and yMax
+    }
+}
