412cfaadd33964f77a2914d60f45e5721e55896f
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-10-11 18:23:30 -0400

    Laser and Collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[System.Serializable]
+public class Boundary
+{
+    // Properties ( Variables )
+    public float xMin, xMax, yMin, yMax;
+    // Behaviours ( Function / Methods )
+}
+
+public class PlayerController : MonoBehaviour
+{
 
     // Variable Declaration
+    [Header("Movement Settings")]
     public float speed = 5.0f;
-    public float xMin, xMax, yMin, yMax;
+    // public float xMin, xMax, yMin, yMax;
+    public Boundary boundary;
 
-	// Use this for initialization
-	void Start () {
+    [Header("Attack Settings")]
+    public GameObject laser;
+    public GameObject laserSpawn;
+    public float fireRate = 0.5f;
 
+    private Rigidbody2D rBody;
+    private float myTime = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTime += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && myTime > fireRate)
+        {
+            Instantiate(laser, laserSpawn.transform.position, laserSpawn.transform.rotation);
+            myTime = 0.0f;
+        }
+    }
 
     // Fixed update increments. Used for physics!
     void FixedUpdate()
@@ -24,17 +51,18 @@ public class PlayerController : MonoBehaviour {
         // Read input
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
+
         Debug.Log("x: " + horiz + ", y: " + vert);
 
         Vector2 movement = new Vector2(horiz, vert);
 
         // Move the player
-        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        // Rigidbody2D rBody = GetComponent<Rigidbody2D>();
         rBody.velocity = movement * speed;
 
         // Restrict the player from leaving the play area
         rBody.position = new Vector2(
-            Mathf.Clamp(rBody.position.x, xMin, xMax),  // Restrict the x position to xMin and xMax
-            Mathf.Clamp(rBody.position.y, yMin, yMax)); // Restrict the y position to yMin and yMax
+            Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax),  // Restrict the x position to xMin and xMax
+            Mathf.Clamp(rBody.position.y, boundary.yMin, boundary.yMax)); // Restrict the y position to yMin and yMax
     }
 }
