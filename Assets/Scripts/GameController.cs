77f0c74bf99096b5dc5c16d17c4eb94909768010
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-11-15 17:26:30 -0500

    Waves!

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is where our game logic will be run
+public class GameController : MonoBehaviour {
+    
+    [Header("Wave Settings")]
+    public GameObject hazard;   // What are we spawning?
+    public Vector2 spawn;       // Where do we spawn our hazards?
+    public int hazardCount;     // How many hazards per wave?
+    public float startWait;     // How long until the first wave?
+    public float spawnWait;     // How long betwen each hazard in each wave?
+    public float waveWait;      // How long between each waves?
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        StartCoroutine(SpawnWaves());
+        gameOver = false;
+        restart = false;
+	}
+	
+    // Function dedicated to spawning waves of hazards
+    // Coroutine
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait); // Pause. This will "wait" for "startWait" seconds
+        while(true)
+        {
+            // Spawning our hazards
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector2 spawnPosition = new Vector2(spawn.x, Random.Range(-spawn.y, spawn.y));
+                //                                    11                     -4         4
+                Instantiate(hazard, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
